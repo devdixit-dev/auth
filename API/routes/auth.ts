d65755e7fef950bc6cf6e466fbff7bb037d1725e
Dev Dixit <devdixitsocial@gmail.com>
2025-11-01 21:39:28 +0530

    - modify Verify and Logout route and add isAuth protection

--- API/routes/auth.ts
+++ API/routes/auth.ts
@@ -4,15 +4,16 @@ import { validate } from "../middlewares/validator";
 import { RegisterSchema, VerifySchema, SignInSchema } from "../validators/user";
 
 import { AuthLogout, AuthRegister, AuthSignIn, AuthVerify } from "../controllers/auth";
+import isAuth from "../middlewares/isAuth";
 
 const AuthRouter = Router();
 
 AuthRouter.get('/register', validate(RegisterSchema), AuthRegister);
 
-AuthRouter.post('/verify', validate(VerifySchema), AuthVerify);
+AuthRouter.post('/verify', isAuth, validate(VerifySchema), AuthVerify);
 
 AuthRouter.post('/signin', validate(SignInSchema), AuthSignIn);
 
-AuthRouter.post('/logout', AuthLogout);
+AuthRouter.post('/logout', isAuth, AuthLogout);
 
 export default AuthRouter;
